67c9c6376b163d4ca1e0c18c962d866b8c4b6850
jw <industrialdonut@gmail.com>
2024-03-06 18:38:46 -0800

    regenerated and re-signed

--- generate.py
+++ generate.py
@@ -1,10 +1,21 @@
 from pki_helpers import generate_private_key, generate_public_key, generate_csr
 
 
-private_key = generate_private_key('server-private-key.pem', 'server_password') # secret_password for CA and server_password for SERVER 's private keys on disk
+ca_private_key = generate_private_key('ca-private-key.pem', 'secret_password')
+generate_public_key(
+    private_key=ca_private_key,
+    filename='ca-public-key.pem',
+    country='US',
+    state='CA',
+    locality='LA',
+    org='donut',
+    hostname='donut.com',
+)
 
+
+server_private_key = generate_private_key('server-private-key.pem', 'server_password')
 generate_csr(
-    private_key=private_key,
+    private_key=server_private_key,
     filename='server-csr.pem',
     country='US',
     state='CA',
@@ -14,12 +25,3 @@ generate_csr(
     hostname='donut.com',
 )
 
-generate_public_key(
-    private_key=private_key,
-    filename='ca-public-key.pem',
-    country='US',
-    state='CA',
-    locality='LA',
-    org='donut',
-    hostname='donut.com',
-)
